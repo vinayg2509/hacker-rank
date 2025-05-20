fd8bd3e7a43f8a58c2f438284edafaaa2f59b770
vinayg2509 <vinayg2509@gmail.com>
2025-05-20 22:09:44 +0530

    [Vinay]: Add program to read a String and two integers from user input and print them

---
+++ src/main/java/com/bridgelabz/hackerrank/ReadIntegers.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.hackerrank;
+
+import java.util.Scanner;
+
+public class ReadIntegers
+{
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        String myString= scanner.next();
+        int myInt=scanner.nextInt();
+        int myNew=scanner.nextInt();
+
+        System.out.println(myString);
+        System.out.println(myInt);
+        System.out.println(myNew);
+    }
+}
