c27bb964ad89544a5e89d59f815972201a730900
vinayg2509 <vinayg2509@gmail.com>
2025-05-21 15:13:30 +0530

    [Vinay] Add program to format and print strings with left alignment and zero-padded integers

---
+++ src/main/java/com/bridgelabz/hackerrank/FormattedOutput.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.hackerrank;
+
+import java.util.Scanner;
+
+public class FormattedOutput
+{
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=scanner.next();
+            int x=scanner.nextInt();
+            //Complete this line
+            System.out.printf("%-15s%03d%n",s1,x);
+        }
+        System.out.println("================================");
+
+    }
+}
