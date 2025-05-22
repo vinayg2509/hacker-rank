b01c121dc72f249ce52ab570fb743d8ace2ab50b
vinayg2509 <vinayg2509@gmail.com>
2025-05-22 22:55:56 +0530

    [Vinay]Delete:Deleted some unwanted lines

--- src/main/java/com/bridgelabz/hackerrank/EndOfFileReader.java
+++ src/main/java/com/bridgelabz/hackerrank/EndOfFileReader.java
@@ -1,4 +1,17 @@
 package com.bridgelabz.hackerrank;
 
-public class EndOfFileReader {
+import java.util.Scanner;
+
+public class EndOfFileReader
+{
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        int n=1;
+
+        while (scanner.hasNext())
+        {
+            System.out.println(n +" "+scanner.nextLine());
+            n++;
+        }
+    }
 }
