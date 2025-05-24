9d14297b1adc758bec5ad9de702215631302bca6
vinayg2509 <vinayg2509@gmail.com>
2025-05-24 15:05:39 +0530

    [Vinay]add manual isAnagram method using character frequency comparison without sorting

--- src/main/java/com/bridgelabz/hackerrank/StringAnagramCheck.java
+++ src/main/java/com/bridgelabz/hackerrank/StringAnagramCheck.java
@@ -1,4 +1,40 @@
 package com.bridgelabz.hackerrank;
 
-public class StringAnagramCheck {
+import java.util.Scanner;
+
+public class StringAnagramCheck
+{
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        String s1=scanner.nextLine();
+        String s2=scanner.nextLine();
+        System.out.println(isAnagram(s1,s2)?"Anagram":"Not Anagram");
+
+    }
+
+    static boolean isAnagram(String a, String b) {
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            count[a.charAt(i) - 'a']++;
+            count[b.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
